9f26a279a68b7283dc47e6c84be3fc6137e4540d
Alexsey Shestacov <wingrime@gmail.com>
2016-01-30 17:43:04 +0500

    Dict based answers
    Now just send messages form dict

--- MasterPickupBot.go
+++ MasterPickupBot.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/tucnak/telebot"
@@ -16,7 +19,17 @@ var (
 	logger = log.New(os.Stderr, "[main] ", log.LstdFlags)
 )
 
+func processGeneralMessage(str string, dict []string) string {
+	return "Он сказал: " + dict[rand.Intn(len(dict))]
+}
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	dictData, err := ioutil.ReadFile("dict.txt")
+	if err != nil {
+		logger.Fatal(err)
+	}
+	dict := strings.Split(string(dictData), "\n")
+
 	SecretAPIKey := os.Getenv(BotApiKey)
 	if SecretAPIKey == "" {
 		logger.Fatalf("env variable %s not specifited", BotApiKey)
@@ -28,10 +41,21 @@ func main() {
 
 	messages := make(chan telebot.Message)
 	bot.Listen(messages, 1*time.Second)
+	sendOptions := telebot.SendOptions{
+		ReplyMarkup: telebot.ReplyMarkup{
+			ForceReply: true,
+			Selective:  true,
 
+			CustomKeyboard: [][]string{
+				[]string{"Совет Гуру"},
+			},
+		},
+	}
 	for message := range messages {
 		if message.Text == "/start" {
-			bot.SendMessage(message.Chat, "Привет, я раскажу тебе как двигать тазом, "+message.Sender.FirstName+"!", nil)
+			bot.SendMessage(message.Chat, "Привет, я раскажу тебе как двигать тазом, "+message.Sender.FirstName+"!", &sendOptions)
+		} else {
+			bot.SendMessage(message.Chat, processGeneralMessage(message.Text, dict), &sendOptions)
 		}
 	}
 }
