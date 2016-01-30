e91e44b4fd49c40b75dbf4ef1840cdaaa52b4545
Alexsey Shestacov <wingrime@gmail.com>
2016-01-30 13:47:53 +0500

    Simple initial implementation
    Initial working implementation

--- MasterPickupBot.go
+++ MasterPickupBot.go
@@ -1,1 +1,37 @@
-//initial commit
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/tucnak/telebot"
+)
+
+const (
+	BotApiKey = "API_KEY" // "Telegram API Key"
+)
+
+var (
+	logger = log.New(os.Stderr, "[main] ", log.LstdFlags)
+)
+
+func main() {
+	SecretAPIKey := os.Getenv(BotApiKey)
+	if SecretAPIKey == "" {
+		logger.Fatalf("env variable %s not specifited", BotApiKey)
+	}
+	bot, err := telebot.NewBot(SecretAPIKey)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	messages := make(chan telebot.Message)
+	bot.Listen(messages, 1*time.Second)
+
+	for message := range messages {
+		if message.Text == "/start" {
+			bot.SendMessage(message.Chat, "Привет, я раскажу тебе как двигать тазом, "+message.Sender.FirstName+"!", nil)
+		}
+	}
+}
